e278486a87e2a3687686b64ea0ef0e8462fb6df6
Aditya219 <56546961+Aditya219@users.noreply.github.com>
2019-10-21 18:39:46 +0530

    except sidebar all done

--- User_End/leave/leave.js
+++ User_End/leave/leave.js
@@ -1,16 +1,32 @@
-alert("sucess");
-
+//submit function
 var sub = document.getElementById("sub");
 sub.addEventListener("click", submitFun);
 
-
-
-
-//submit function
 function submitFun(){
     var conformation = confirm("Do you want to submit");
     if(conformation)
     {
-               
+         document.getElementById("form-action").action = "../profile/profile.html";      
+    }
+    else
+    {
+        //how??????????????????????????? without changing the current inputs
+    }
+}
+
+//cancel function
+var can = document.getElementById("cancel")
+cancel.addEventListener("click", cancelFun);
+
+function cancelFun()
+{
+    var conformation = confirm("Data may lost.");
+    if(conformation)
+    {
+        document.getElementById("cancel").href = "../profile/profile.html";
+    }
+    else
+    {
+        //how??????????????????????????? without changing the current inputs
     }
 }
